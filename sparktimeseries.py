63721fa589ea2145d1189c79eeb022103255aec9
Ravi Gadde <rtg267@login-1-1.local>
2018-04-10 20:59:51 -0400

    Filling missing values in timeseries and create a Json containing all timeseries data

--- sparktimeseries.py
+++ sparktimeseries.py
@@ -56,7 +56,7 @@ def processtimeseries(chart_events, icu_static_dict, item_ids):
 	chart_events = chart_events.groupByKey().mapValues(aggregatetimeseries)
 
 	#icu_id, item_id --> value, charttimeindex
-	chart_events = chart_events.map(lambda x: ((x[0][0], x[0][1]), (x[1], x[0][2])))	
+	chart_events = chart_events.map(lambda x: ((x[0][0], x[0][1]), [x[1], x[0][2]]))	
 
 	chart_events = chart_events.groupByKey().mapValues(list)	
 
@@ -66,7 +66,27 @@ def processtimeseries(chart_events, icu_static_dict, item_ids):
 
 	chart_events = chart_events.map(sortlist)	
 
-	chart_events.map(lambda x: "{0},{1}\t{2}".format(x[0][0], x[0][1], x[1])).saveAsTextFile("icu_timeseries.out")
+	def expandtimeseries(x):
+		missing_initial = -2
+		missing_middle = -1
+		time_series_expand = []
+		previous_timeindex = -1
+		for value, timeindex in x[1]:
+			if timeindex <= previous_timeindex:
+				continue
+			while(timeindex != previous_timeindex + 1):
+				if previous_timeindex == -1:
+					time_series_expand.append([missing_initial, previous_timeindex+1])
+				else:
+					time_series_expand.append([missing_middle, previous_timeindex+1])
+				previous_timeindex = previous_timeindex + 1
+			previous_timeindex = previous_timeindex + 1
+			time_series_expand.append([value,timeindex])
+		return (x[0], time_series_expand)
+
+	chart_events = chart_events.map(expandtimeseries)
+
+	chart_events.map(lambda x: "{0}\"({1},{2})\" : {3}{4}".format('{', x[0][0], x[0][1], x[1], '}')).saveAsTextFile("icu_timeseries.out")
 
 	#chart_events = chart_events.map(lambda x: (x[0][0], 1)).groupByKey().mapValues(len)
 	#chart_events.map(lambda x: "{0},{1}".format(x[0], x[1])).saveAsTextFile("ravi.out")
