9c755b76b1a217493d36f2c63a13dc727d5e86cc
raviatnyu <xravitejax@gmail.com>
2018-04-27 20:22:39 -0400

    OutlierDetect Updated

--- outlierdetect.py
+++ outlierdetect.py
@@ -3,20 +3,26 @@ import sys
 import json
 
 def detectoutliers(labstimeseriesfile):
-	outliers = []
+	outliers = {}
+	outliersslim = {}
 	labsfile = open(labstimeseriesfile, 'r')
 	for line in labsfile:
 		line = line.strip().replace('\'', '\"')
 		line = json.loads(line)
 		for key, valuelist in line.items():
+			featurename = key[1:-1].split(',')[1]
+			if featurename not in outliers:
+				outliers[featurename] = []
 			for value in valuelist:
 				try: float(value[0])
-				except: outliers.append(value[0])
-	return list(set(outliers))
+				except: outliers[featurename].append(value[0])
+	for key,valuelist in outliers.items():
+		outliersslim[key] = list(set(valuelist))
+	return outliers, outliersslim
 
 if __name__=='__main__':
 	labstimeseriesfile = sys.argv[1]
-	outliers = detectoutliers(labstimeseriesfile)
+	outliers, outliersslim = detectoutliers(labstimeseriesfile)
 	print(outliers)
-	print(len(outliers))
-
+	print(outliersslim)
+#python outlierdetect.py icu_timeseries_labs.txt
